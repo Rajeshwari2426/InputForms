015c603b84fc0a8dc9e0c99fddb59a2820a50ddd
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-03-16 22:48:23 +0530

    [Rajeshwari]adding HTML5 InputForms Validate First name

---
+++ Js/Inputform.js
@@ -0,0 +1,15 @@
+//uc1 first name start with capital letter and min 3 letter
+const text = document.querySelector('#text');               //storing the value using id = text
+const textError = document.querySelector('.text-error');    //similarly getting output for texterror 
+text.addEventListener('input', function () {
+    let nameRegex = RegExp('^[A-Z]{1}[A-Za-z]{2,}$');
+    if (nameRegex.test(text.value) || !text.value)
+    {
+        textError.textContent = "";
+    }
+    else 
+    {
+        textError.textContent = "Enter a Valid Name";
+        console.log("note:first name start with capital letter and min 3 letter")
+    }
+});
